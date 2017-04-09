08935f6a5913c27cc96c031f387f2598ff6567e3
lugowskii <lugowskii.piotr@gmail.com>
2017-04-09 14:32:59 +0200

    Only tables mapped with hibernate are loaded.

--- src/main/java/com/bsk/controllers/DashboardController.java
+++ src/main/java/com/bsk/controllers/DashboardController.java
@@ -3,20 +3,29 @@ package com.bsk.controllers;
 
 import com.bsk.domain.Customer;
 import com.bsk.services.CustomerService;
+import org.hibernate.Session;
+import org.hibernate.metadata.ClassMetadata;
+import org.hibernate.persister.entity.AbstractEntityPersister;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+import javax.persistence.EntityManager;
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
 
 @Controller
 public class DashboardController {
 
     private CustomerService customerService;
 
-    public DashboardController(CustomerService customerService) {
+    private EntityManager entityManager;
+
+    public DashboardController(CustomerService customerService, EntityManager entityManager) {
         this.customerService = customerService;
+        this.entityManager = entityManager;
     }
 
     @GetMapping("/dashboard")
@@ -28,5 +37,18 @@ public class DashboardController {
     @ResponseBody
     public List<Customer> getCustomers() {
         return customerService.getCustomers();
+    }
+
+    @ModelAttribute("tables")
+    @ResponseBody
+    public List<String> getTables() {
+        Session session = entityManager.unwrap(Session.class);
+        Map<String, ClassMetadata> hibernateMetadata = session.getSessionFactory().getAllClassMetadata();
+        List<String> tableNames = new ArrayList<>();
+        for (ClassMetadata classMetadata : hibernateMetadata.values()) {
+            AbstractEntityPersister aep = (AbstractEntityPersister) classMetadata;
+            tableNames.add(aep.getTableName());
+        }
+        return tableNames;
     }
 }
