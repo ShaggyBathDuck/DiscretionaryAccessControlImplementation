3020ed05fbf1687134222c53e419bb22920403b3
lugowskii <lugowskii.piotr@gmail.com>
2017-04-09 14:08:53 +0200

    CustomerController provided

--- src/main/java/com/bsk/controllers/DashboardController.java
+++ src/main/java/com/bsk/controllers/DashboardController.java
@@ -3,22 +3,19 @@ package com.bsk.controllers;
 
 import com.bsk.domain.Customer;
 import com.bsk.services.CustomerService;
-import com.bsk.services.UserService;
-import com.bsk.domain.User;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 import java.util.List;
 
 @Controller
 public class DashboardController {
 
-    private UserService userService;
     private CustomerService customerService;
 
-    public DashboardController(UserService userService, CustomerService customerService) {
-        this.userService = userService;
+    public DashboardController(CustomerService customerService) {
         this.customerService = customerService;
     }
 
@@ -27,29 +24,9 @@ public class DashboardController {
         return "dashboard";
     }
 
-    @ModelAttribute("allUsers")
-    @ResponseBody
-    public List<User> getUsers(){
-        return userService.getUsers();
-    }
-
     @ModelAttribute("allCustomers")
     @ResponseBody
-    public List<Customer> getCustomers(){
+    public List<Customer> getCustomers() {
         return customerService.getCustomers();
-    }
-
-    @PostMapping(value = "/dashboard/add")
-    public String add(Customer customer, Model model){
-        customerService.add(customer);
-        model.addAttribute("allCustomers", customerService.getCustomers());
-        return "redirect:/dashboard";
-    }
-
-    @DeleteMapping(value = "/dashboard/delete/{nip}")
-    public String delete(@PathVariable String nip, Model model){
-        customerService.remove(nip);
-        model.addAttribute("allCustomers", customerService.getCustomers());
-        return "redirect:/dashboard";
     }
 }
