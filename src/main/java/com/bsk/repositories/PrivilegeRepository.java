356f2a2ab4b0bca59eb4f8e85b8f0408964d018e
ShaggyBathDuck <dawid.kaszubski66@gmail.com>
2017-05-15 23:17:55 +0200

    Privilege repository

---
+++ src/main/java/com/bsk/repositories/PrivilegeRepository.java
@@ -0,0 +1,8 @@
+package com.bsk.repositories;
+
+
+import com.bsk.domain.Privilege;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface PrivilegeRepository extends JpaRepository<Privilege, Long> {
+}
